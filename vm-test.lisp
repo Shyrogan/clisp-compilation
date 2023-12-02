7128c01fe04666b085addc2b4c3dc07635c3c0df
SateaMall <75503482+SateaMall@users.noreply.github.com>
2023-12-02 17:29:37 +0100

    modify the test to be readable

--- vm-test.lisp
+++ vm-test.lisp
@@ -1,28 +1,37 @@
 (require "vm.lisp")
 
 
+ 
   ;; Créer une instance de la VM et l'initialiser
 (let  ((vm '()))
+      
       (vm-init vm)
-      (print (= (get vm :FGT ) 0)) ;; pour voir si les valeur on était initialiser
+      
+      (print '( test vm-init :) )
+      (print (= (get vm :FGT ) 0)) ;; test init
       
       (setf (get vm :PC) 5) ;; on modifie pc
       (vm-reset vm)
-      (print  (= (get vm :PC ) 999) ) ;; pour voir que vm-reset a fait son travail
+      
+      (print '( test vm-resert :) )
+      (print  (= (get vm :PC ) 999) ) ;; test execute
   
-      ;; Charger un programme de test dans la VM
+ 
       (vm-load vm '((LOAD :R1 10 ) (STORE :R1 10 ) ))
-      ;; Exécuter le programme (supposant que vous avez une fonction vm-execute)
       (vm-execute  vm)
 
       ;; Vérifier les résultats
+      (print '( tests for load and execute :) )
       (print (= (get vm :R1 ) 10)) 
       (print (= (get-mem vm 10) 10 )) 
       
+      (print '( cant print the vm dont know why :) )
       (print vm) ;; ca marche pas!!!!!!!!!!!!!!!!!!!!!!
       (afficher-registres vm) ;; ca marche pas!!!!!!!!!!!!!!!!!!!!!! jppp!
 
   )
+
+
 
 
 
